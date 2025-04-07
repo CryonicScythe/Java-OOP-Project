a8486c2c708e89f816fc0005433c4ac78ab04231
Taylor_Alexander <tayla203@hsd.k12.or.us>
2025-04-07 09:32:05 -0700

    added GUI screen

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -2,8 +2,12 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
+    Button startButton = new Button();
+    Button storeButton = new Button();
+    Button inventoryButton = new Button();
 
     @FXML
     private void switchToSecondary() throws IOException {
