1d236054f42ab8a10e636ad8ed79598dca6611f2
Taylor_Alexander <tayla203@hsd.k12.or.us>
2025-04-09 10:05:37 -0700

    added fight page to game after pressing start

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -16,11 +16,6 @@ public class PrimaryController {
     
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
-
-    @FXML
-    private void switchToThirtiary() throws IOException {
-        App.setRoot("thirtiary");
+        App.setRoot("secondaryController");
     }
 }
--- src/main/java/com/example/SecondaryController.java
+++ src/main/java/com/example/SecondaryController.java
@@ -2,9 +2,23 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class SecondaryController {
 
+    @FXML
+    Label outpLabel = new Label();
+
+    @FXML
+    Button fightButton = new Button();
+
+    @FXML
+    Button escapeButton = new Button();
+
+    @FXML
+    Button inventoryButton = new Button();
+    
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primaryController");
